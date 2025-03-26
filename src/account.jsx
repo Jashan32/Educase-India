1f0e908a44e6d455fe257077b0913512396ba2c8
Jashan32 <jashandeep1849@gmail.com>
2025-03-26 21:55:01 +0530

    fixed text size

--- src/account.jsx
+++ src/account.jsx
@@ -1,13 +1,13 @@
 function Account() {
     return (
         <div className="w-[100vw] flex justify-center h-[100dvh] items-center">
-            <div className="xl:h-[90%] h-[100dvh] border border-gray-200 flex flex-col">
+            <div className="xl:h-[90%] h-[100dvh]  flex flex-col">
 
-                <div className="shadow-md z-10 shadow-b-black font-semibold h-[68px] flex items-center text-[18px] text-[#1D2226] pl-[20px]">
+                <div className="shadow-b-sm z-10 shadow-b-black font-normal h-[68px] flex items-center text-[18px] text-[#1D2226] pl-[20px]">
                     Account Settings
                 </div>
 
-                <div className="max-w-[100vw] w-[375px] h-full bg-[#F7F8F9] flex flex-col justify-start pt-[30px]">
+                <div className="border border-gray-200 max-w-[100vw] w-[375px] h-full bg-[#F7F8F9] flex flex-col justify-start pt-[30px]">
 
                     <div className="px-[20px] flex gap-[20px] relative">
                         <img src="./pimg.png" className="h-[76px]" />
@@ -18,7 +18,7 @@ function Account() {
                         </div>
                     </div>
 
-                    <div className="px-[20px] text-[#1D2226]/60 mt-[30px] pb-[10px]">
+                    <div className="px-[20px] text-[13px] text-[#1D2226] mt-[30px] pb-[10px]">
                         Lorem Ipsum Dolor Sit Amet, Consetetur Sadipscing Elitr, Sed Diam Nonumy Eirmod Tempor Invidunt Ut Labore Et Dolore Magna Aliquyam Erat, Sed Diam
                     </div>
 
